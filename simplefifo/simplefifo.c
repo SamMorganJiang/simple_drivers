da115abfb1867abc1912a899bf05b7f25f94cb25
jiangshan <jiangshan@vanzotec.cn>
2018-07-02 11:49:55 +0800

    update file operations

--- simplefifo/simplefifo.c
+++ simplefifo/simplefifo.c
@@ -10,8 +10,13 @@
 #include <linux/module.h>
 #include <linux/types.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/sched.h>
+#include <linux/errno.h>
+#include <linux/mm.h>
 #include <linux/miscdevice.h>
 #include <linux/platform_device.h>
+#include <asm/uaccess.h>
 
 #define SIMPLEFIFO_NAME     "simplefifo"
 #define SIMPLEFIFO_SIZE     0x1000
@@ -25,7 +30,7 @@ static struct simplefifo_dev {
 struct simplefifo_dev *sf;
 
 static int simplefifo_open(struct inode *inode, struct file *filp) {
-    filp->private_data = sf;
+    filp->private_data = (void *)sf;
     return 0;
 }
 
@@ -36,32 +41,111 @@ static int simplefifo_release(struct inode *inode, struct file *filp) {
 static ssize_t simplefifo_read(struct file *filp, char __user *buf,
 			       size_t count, loff_t *ppos)
 {
-    return 0;
+    ssize_t ret = 0;
+    struct simplefifo_dev *dev = filp->private_data;
+
+    if (*ppos >= SIMPLEFIFO_SIZE)
+        return 0;
+    if (count > SIMPLEFIFO_SIZE - *ppos)
+        count = SIMPLEFIFO_SIZE - *ppos;
+
+    if (copy_to_user(buf, dev->mem + *ppos, count))
+        return -EFAULT;
+    else {
+        *ppos += count;
+        ret = count;
+        pr_info(KERN_INFO "read %u bytes form %lu\n", count, *ppos);
+    }
+
+    return ret;
 }
 
 static ssize_t simplefifo_write(struct file *filp, const char __user *buf,
 				size_t count, loff_t *ppos)
 {
-    return 0;
+    ssize_t ret = 0;
+    struct simplefifo_dev *dev = filp->private_data;
+
+    if (*ppos >= SIMPLEFIFO_SIZE)
+        return 0;
+    if (count > SIMPLEFIFO_SIZE - *ppos)
+        count = SIMPLEFIFO_SIZE - *ppos;
+
+    if (copy_from_user(dev->mem + *ppos, buf, count))
+        return -EFAULT;
+    else {
+        *ppos += count;
+        ret = count;
+        pr_info(KERN_INFO "write %u bytes to %lu\n", count, *ppos);
+    }
+
+    return ret;
 }
 
 static long simplefifo_ioctl(struct file *filp, unsigned int cmd,
 			     unsigned long arg)
 {
+    struct simplefifo_dev *dev = filp->private_data;
+
+    switch (cmd) {
+        case FIFO_CLEAR:
+            memset(dev->mem, 0, SIMPLEFIFO_SIZE);
+            pr_info(KERN_INFO "simplefifo is cleared\n");
+            break;
+
+        default:
+            return -EINVAL;
+    }
+
     return 0;
 }
 
+static loff_t simplefifo_llseek(struct file *filp, loff_t offset, int orig) {
+    loff_t ret = 0;
+
+    switch (orig) {
+        case 0:
+            if (offset < 0) {
+                ret = -EINVAL;
+                break;
+            }
+            if (offset > SIMPLEFIFO_SIZE) {
+                ret = -EINVAL;
+                break;
+            }
+            filp->f_pos = offset;
+            ret = filp->f_pos;
+            break;
+
+        case 1:
+            if (filp->f_pos + offset > SIMPLEFIFO_SIZE || filp->f_pos + offset < 0) {
+                ret = -EINVAL;
+                break;
+            }
+            filp->f_pos += offset;
+            ret = filp->f_pos;
+            break;
+
+        default:
+            ret = -EINVAL;
+            break;
+    }
+
+    return ret;
+}
+
 static const struct file_operations simplefifo_fops = {
-	.owner = THIS_MODULE,
-	.open = simplefifo_open,
-	.release = simplefifo_release,
-	.read = simplefifo_read,
-	.write = simplefifo_write,
-	.unlocked_ioctl = simplefifo_ioctl,
+    .owner = THIS_MODULE,
+    .open = simplefifo_open,
+    .release = simplefifo_release,
+    .read = simplefifo_read,
+    .write = simplefifo_write,
+    .unlocked_ioctl = simplefifo_ioctl,
+    .llseek = simplefifo_llseek,
 };
 
 static int simplefifo_probe(struct platform_device *pdev) {
-    int ret;
+    int ret = 0;
 
     sf = devm_kzalloc(&pdev->dev, sizeof(*sf), GFP_KERNEL);
     if (!sf)
@@ -76,7 +160,6 @@ static int simplefifo_probe(struct platform_device *pdev) {
         goto err;
 
     dev_info(&pdev->dev, "simplefifo probe ok!\n");
-    return 0;
 err:
     return ret;
 }
